8667eeb08c709e0b955aa286dc2cacfcaff0fa80
hyunjin lee <limgx5192@gmail.com>
2022-10-26 10:39:54 +0900

    메인페이지 전달

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postList from "../modules/postSlice";
 import commentList from "../modules/commentSlice";
-import userList from "../moudule/userSlice";
 import detailPost from "../modules/detailPostSlice";
+import userList from "../modules/userSlice";
 
 const store = configureStore({
      reducer: { postList, commentList, userList, detailPost },
