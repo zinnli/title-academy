d32bc303703607687b3ee155aa4804b220b7fb59
hyunjin lee <limgx5192@gmail.com>
2022-10-27 17:21:16 +0900

    http 오류 롤백

--- src/shared/request.js
+++ src/shared/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-     baseURL: "https://54.180.26.228:8080",
+     baseURL: "http://54.180.26.228:8080",
 });
 
 // axiosInstance.defaults.headers.common["Authorization"] = USER_TOKEN;
